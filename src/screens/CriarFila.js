6c0c2842b4246d8389853eb86ee72a04be0286ad
Matheus-Sampaio <matheusballestra@hotmail.com>
2020-06-09 17:36:26 -0300

    debbuging the queue creation bug

--- src/screens/CriarFila.js
+++ src/screens/CriarFila.js
@@ -2,12 +2,15 @@ import * as React from 'react'
 import { Text, View, TextInput} from 'react-native'
 import { Formik } from 'formik'
 
+
 // Styles
 import CriarFilaStyles from '../styles/CriarFilaStyles'
 
 // Components
 import Button from '../components/Button'
 import Input from '../components/Input'
+import filasApi from '../axios/Axios'
+import { getFila } from '../axios/Axios'
 
 const CriarFila = ({ navigation }) => {
   return (
@@ -16,9 +19,12 @@ const CriarFila = ({ navigation }) => {
         initialValues={{ nomeFila: '' }}
         onSubmit={async values => {
           await filasApi.post('/cria_fila', {
-            filaName: values
-          }).then((res) => {
-            navigation.navigate('FilaUser', { id: fila.id })
+            nomeFila: values.nomeFila
+          }).then(async (res) => {
+                await filasApi.get('/lista_filas').then((res)=>{
+                  const fila = getFila(res.data, values)
+                  navigation.navigate('FilaUser', { id: fila.id })
+                }).catch((err) => {console.log(err)})
           }).catch((err) => {
             // handle error
             console.log(err);
@@ -29,11 +35,11 @@ const CriarFila = ({ navigation }) => {
         <View style={CriarFilaStyles.tpForm}>
           <Text>Digite o nome da Fila:</Text>
            <Input
-              onChangeText={handleChange('senhaFila')}
-              onBlur={handleBlur('NomeFila')}
+              onChangeText={handleChange('nomeFila')}
+              onBlur={handleBlur('nomeFila')}
               value={values.nomeFila}
            />
-         <Button onPress={handleSubmit} text='Enviar' />
+         <Button onPress={handleSubmit} text='Criar Fila' />
         </View>
        )}
       </Formik>
